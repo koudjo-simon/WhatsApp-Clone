ee97627abb5c0936dafab8832c9f691d62f3c24b
Koudjo Simon ODOH <pro.simonkoudjo.9204@gmail.com>
2023-07-06 18:58:39 +0200

    Ajout de l'evenement de click sur l'icone tel

--- app/src/main/java/oks/ro/application2/StudentAdapter.kt
+++ app/src/main/java/oks/ro/application2/StudentAdapter.kt
@@ -1,6 +1,7 @@
 package oks.ro.application2
 
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import oks.ro.application2.model.Student
+
 
 class StudentAdapter(var studentList: List<Student>) :
     RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
@@ -24,12 +26,20 @@ class StudentAdapter(var studentList: List<Student>) :
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
         val student = studentList[position]
+
         holder.update(student)
 
         holder.row.setOnClickListener {
             val intent = Intent(holder.itemView.context, SettingsActivity::class.java)
             intent.putExtra("USERNAME", student.fullName)
             holder.itemView.context.startActivity(intent)
+        }
+
+        holder.phoneNumber.setOnClickListener{
+            val callNumber = student.phone
+            val number = Uri.parse("tel:$callNumber")
+            val callIntent = Intent(Intent.ACTION_DIAL, number)
+            holder.itemView.context.startActivity(callIntent)
         }
     }
 
